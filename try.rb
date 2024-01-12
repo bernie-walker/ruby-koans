6d34258009d467ccbc76c2b731f6fc8584bd5993
Bernard Nehal D'Cunha <bernie-walker@users.noreply.github.com>
2024-01-12 15:24:34 +0530

    finished about_proxy_object_project.rb

--- try.rb
+++ try.rb
@@ -19,10 +19,28 @@ class Human
   def get_legs
     LEGS
   end
+
+  def bloke(name)
+    "Hiya #{name}"
+  end
+
+  def proxy_to_send(method_name, *args)
+    self.send(method_name, *args)
+  end
+
 end
 
 human = Human.new
 
-puts human.get_legs
-puts Human.speak
-puts Animal.legs
+# puts human.get_legs
+# puts Human.speak
+# puts human.bloke("Mark")
+puts human.proxy_to_send(:bloke, "Maximilian")
+# puts Animal.legs
+
+
+def method_with_varargs(*args)
+  args
+end
+
+# puts method_with_varargs(5, one: 1, two: 2).inspect
